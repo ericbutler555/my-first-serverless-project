a9b3403c7db0110bc3a4829a0cb630a5712c5b1e
Eric Butler <ericbutler555@gmail.com>
2021-11-01 14:41:38 -0400

    Create pokedex function with API call

--- netlify/functions/pokedex.js
+++ netlify/functions/pokedex.js
@@ -1,8 +1,12 @@
+const fetch = require('node-fetch');
+
 exports.handler = async () => {
+  const POKE_API = "https://pokeapi.co/api/v2/pokedex/kanto";
+  const response = await fetch(POKE_API);
+  const data = await response.json();
 
-	// do stuff
-
-	return {
-		// send response back
-	};
-}
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data),
+  };
+};
